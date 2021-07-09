abdfefec5454f457f8511cf0c537401070eb037c
Kaiying Fu <kaiying.fu@gmail.com>
2021-07-09 11:23:15 +0800

    Add Credits to Sidebar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import CVForm from './components/CVForm';
+import githubMark from './assets/github-mark.png'
 import './styles/App.css'
 
 function App() {
   return (
     <div className="app-wrapper">
-      <div className="sidebar">Some Cool Button to Export PDF (coming soon)</div>
+      <div className="sidebar">
+        <p>Some Cool Button to Export PDF (coming soon)</p>
+        <div className="credits-wrapper">
+          <img src={githubMark} alt="GitHub Mark" className="github-mark" />
+          <div>
+            <a href="https://github.com/grumbeard/cv-generator">Visit Repository</a>
+          </div>
+        </div>
+      </div>
       <CVForm />
     </div>
   );
